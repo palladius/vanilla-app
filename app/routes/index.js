63a18f34c2f71b5ba2aa1281a6d3b7da6b6aed50
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-12-03 11:02:14 +0100

    back to business

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+//https://melvingeorge.me/blog/get-all-the-contents-from-file-as-string-nodejs
+const fs = require("fs");
+const version_buffer = fs.readFileSync("VERSION");
+
 //require('dotenv').config();
 var dotenv = require('dotenv')
-var dotenvExpand = require('dotenv-expand')
+var dotenvExpand = require('dotenv-expand');
+const { hostname } = require('os');
 var dotenvConfig = require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  require('os');
   res.render('index', {
-    title: 'Simple Expresso App',
-    appname: '☕ Express0',
+    title: 'Simple ☕ Express0 App',
+    app_name: '☕ Express0',
     dotenv: dotenv,
+    app_version: version_buffer,
+    node_env: process.env.NODE_ENV,
+    hostname: require('os').hostname(),
+    // JAVA: InetAddress.getLocalHost().getHostName()
+//, //     location.hostname,
   });
 });
 
