037797fd73ac3e9e527bb40207968d2f28b5a889
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-12-07 11:28:06 +0100

    refreshing/rafactoring the Cloud Run. Which seems to work fine!

---
+++ push-to-cloud-run-via-skaffold.sh
@@ -0,0 +1,13 @@
+
+
+# 1. who is Wietse? My friend who knows Cloud run.
+# 2. What does skaffold run mean? https://skaffold.dev/docs/references/cli/
+
+# 3. IMPORT NOTE: ENV var comes from .envrc via "direnv allow ."
+echo "Building image in $SKAFFOLD_DEFAULT_REPO_FOR_CLOUDRUN and then Deploying to Cloud Run :)"
+skaffold run -p wietse --default-repo $SKAFFOLD_DEFAULT_REPO_FOR_CLOUDRUN
+
+gcloud run services add-iam-policy-binding sku-vanilla-app \
+    --region "$SKAFFOLD_CLOUD_REGION" \
+    --member="allUsers" \
+    --role="roles/run.invoker"
